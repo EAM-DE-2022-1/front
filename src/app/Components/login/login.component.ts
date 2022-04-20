13fc483aeb34636f612cb3a312e05ad9cbfbb8c5
Juan Esteban Barrero <juanestebanbarrero@hotmail.com>
2022-04-19 20:23:46 -0500

    Se organizan vistas de temperatura y el login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -22,40 +22,40 @@ export class LoginComponent implements OnInit {
     this.usuario = new Usuario();
   }
 
-  // iniciarSesion(form: NgForm) {
-  //   if (form.invalid) {
-  //     this.emptyFieldsError = true;
-  //     this.loginError = false;
-  //     return;
-  //   } else {
-  //     this.service.iniciarSesion(this.usuario)
-  //       .subscribe(
-  //         res => {
-  //           // Splittin token and storing as cookie
-  //           this.cookieService.set('token', this.splitToken(res.token.toString()).toString());
-  //           // Validating authorities
-  //           const responseAuthority = res.authorities[res.authorities.length - 1].authority;
-  //           if (responseAuthority === "ROLE_ADMIN") {
-  //             this.router.navigateByUrl('configuracionAdministrator');
-  //             this.cookieService.set('role', 'admin');
-  //           } else if (responseAuthority === "ROLE_EMPLEADO") {
-  //             this.router.navigateByUrl('homeUsuario');
-  //             this.cookieService.set('role', 'empleado');
-  //           }
-  //         },
-  //         err => {
-  //           this.emptyFieldsError = false;
-  //           this.loginError = true;
-  //           this.usuario = new Usuario();
-  //           this.router.navigateByUrl('**');
-  //         }
-  //       )
-  //   }
-  // }
-  //
-  // splitToken(token: String) {
-  //   var splittedToken = token.split(" ", 2);
-  //   return splittedToken[splittedToken.length-1];
-  // }
+  iniciarSesion(form: NgForm) {
+    if (form.invalid) {
+      this.emptyFieldsError = true;
+      this.loginError = false;
+      return;
+    } else {
+      this.service.iniciarSesion(this.usuario)
+        .subscribe(
+          res => {
+            // Splittin token and storing as cookie
+            this.cookieService.set('token', this.splitToken(res.token.toString()).toString());
+            // Validating authorities
+            const responseAuthority = res.authorities[res.authorities.length - 1].authority;
+            if (responseAuthority === "ROLE_ADMIN") {
+              this.router.navigateByUrl('configuracionAdministrator');
+              this.cookieService.set('role', 'admin');
+            } else if (responseAuthority === "ROLE_EMPLEADO") {
+              this.router.navigateByUrl('homeUsuario');
+              this.cookieService.set('role', 'empleado');
+            }
+          },
+          err => {
+            this.emptyFieldsError = false;
+            this.loginError = true;
+            this.usuario = new Usuario();
+            this.router.navigateByUrl('**');
+          }
+        )
+    }
+  }
+
+  splitToken(token: String) {
+    var splittedToken = token.split(" ", 2);
+    return splittedToken[splittedToken.length-1];
+  }
 
 }
